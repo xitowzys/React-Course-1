c38138314456116c19f3b3a0fcdf1f9726f841de
Kalinka George <xitowzys@gmail.com>
2023-11-05 20:24:28 +0800

    ✨ feat: Creating a journalItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Button from './components/Button/Button';
+import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
 
   return (
     <> 
       <Button />
+      <JournalItem />
     </>
   );
 }

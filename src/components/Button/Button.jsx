e5fbe14ce7f7769a08f1a8d3a7ed79b89723ec00
Kalinka George <xitowzys@gmail.com>
2023-11-07 21:14:05 +0800

    ✨ feat: Using useState hook

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,17 @@
 import './Button.css';
+import { useState } from 'react';
 
 function Button() {
 
+	const [text, setText] = useState('Сохранить');
+
 	const clicked = () => {
+		setText('Закрыть');
 		console.log('Hello');
 	};
 
 	return (
-		<button onClick={clicked} className='button accent'>Сохранить</button>
+		<button onClick={clicked} className='button accent'>{text}</button>
 	);
 }
 

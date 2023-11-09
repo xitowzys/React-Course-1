3641055873cf85680f86a1ef7eaf966f44b008dd
Kalinka George <xitowzys@gmail.com>
2023-11-09 23:23:38 +0800

    ✨ feat: Using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Button from './components/Button/Button';
 import CardButton from './components/CardButton/CardButton';
@@ -10,27 +10,62 @@ import JournalForm from './components/JournalForm/JournalForm';
 import Body from './layouts/Body/Body';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
 
-const INITIAL_DATA = [
-	{
-		id: 1,
-		title: 'Подготовка к обновлению курсов',
-		text: 'Горные походы открывают удивительные природные ландшафт',
-		date: new Date()
-	},
-	{
-		id: 2,
-		title: 'Поход в годы',
-		text: 'Думал, что очень много времени',
-		date: new Date()
-	}
-];
+// [
+// 	{
+// 		"id": 1,
+// 		"title": "Подготовка к обновлению курсов",
+// 		"text": "Горные походы открывают удивительные природные ландшафты",
+// 		"date": "2023-11-09T00:00:00Z"
+// 	},
+// 	{
+// 		"id": 2,
+// 		"title": "Поход в годы",
+// 		"text": "Думал, что очень много времени",
+// 		"date": "2023-11-09T00:00:00Z"
+// 	}
+// ]
+
+// const INITIAL_DATA = [
+// 	{
+// 		id: 1,
+// 		title: 'Подготовка к обновлению курсов',
+// 		text: 'Горные походы открывают удивительные природные ландшафт',
+// 		date: new Date()
+// 	},
+// 	{
+// 		id: 2,
+// 		title: 'Поход в годы',
+// 		text: 'Думал, что очень много времени',
+// 		date: new Date()
+// 	}
+// ];
 
 function App() {
-	const [items, setItems] = useState(INITIAL_DATA);
+	const [items, setItems] = useState([]);
+
+
+	useEffect(() => {
+
+		const data = JSON.parse(localStorage.getItem('data'));
+
+		if (data) {
+			setItems(data.map(item => ({
+				...item,
+				date: new Date(item.date)
+			})));
+		}
+	}, []);
+
+	useEffect(() => {
+		if (items.length) {
+			console.log('Запись!');
+			localStorage.setItem('data', JSON.stringify(items));
+		}
+	}, [items]);
 
 	const addItem = item => {
 		setItems(oldItems => [...oldItems, {
-			text: item.text,
+			post: item.post,
 			title: item.title,
 			date: new Date(item.date),
 			id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1

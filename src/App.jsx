16d2b80dd7c839a2234e41c3763052462f4ce59a
Kalinka George <xitowzys@gmail.com>
2023-11-10 23:48:44 +0800

    ✨ feat: Custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import JournalList from './components/JournalList/JournalList';
 import JournalForm from './components/JournalForm/JournalForm';
 import Body from './layouts/Body/Body';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
+import { useLocalStorage } from './hooks/use-localstorage.hook';
 
 // [
 // 	{
@@ -40,35 +41,47 @@ import LeftPanel from './layouts/LeftPanel/LeftPanel';
 // 	}
 // ];
 
+function mapItems(items) {
+	if (!items) {
+		return [];
+	}
+	return items.map(i => ({
+		...i,
+		date: new Date(i.date)
+	}));
+}
+
 function App() {
-	const [items, setItems] = useState([]);
+	// const [items, setItems] = useState([]);
 
 
-	useEffect(() => {
+	// useEffect(() => {
 
-		const data = JSON.parse(localStorage.getItem('data'));
+	// 	const data = JSON.parse(localStorage.getItem('data'));
 
-		if (data) {
-			setItems(data.map(item => ({
-				...item,
-				date: new Date(item.date)
-			})));
-		}
-	}, []);
+	// 	if (data) {
+	// 		setItems(data.map(item => ({
+	// 			...item,
+	// 			date: new Date(item.date)
+	// 		})));
+	// 	}
+	// }, []);
 
-	useEffect(() => {
-		if (items.length) {
-			console.log('Запись!');
-			localStorage.setItem('data', JSON.stringify(items));
-		}
-	}, [items]);
+	const [items, setItems] = useLocalStorage('data');
+
+	// useEffect(() => {
+	// 	if (items.length) {
+	// 		console.log('Запись!');
+	// 		localStorage.setItem('data', JSON.stringify(items));
+	// 	}
+	// }, [items]);
 
 	const addItem = item => {
-		setItems(oldItems => [...oldItems, {
+		setItems([...mapItems(items), {
 			post: item.post,
 			title: item.title,
 			date: new Date(item.date),
-			id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1
+			id: items.length > 0 ? Math.max(...items.map(i => i.id)) + 1 : 1
 		}]);
 	};
 
@@ -77,7 +90,7 @@ function App() {
 			<LeftPanel>
 				<Header />
 				<JournalAddButton />
-				<JournalList items={items} />
+				<JournalList items={mapItems(items)} />
 			</LeftPanel>
 			<Body>
 				<JournalForm onSubmit={addItem} />

ddeaf9a9e36355229993c9cb61e61ab3dded4da7
Kalinka George <xitowzys@gmail.com>
2023-11-10 23:19:54 +0800

    ✨ feat: Using forwardRef

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -3,6 +3,7 @@ import Button from '../Button/Button';
 import { useEffect, useReducer, useRef } from 'react';
 import cn from 'classnames';
 import { INITIAL_STATE, formReducer } from './JournalFrom.state';
+import Input from '../Input/Input';
 
 function JournalForm({ onSubmit }) {
 
@@ -64,26 +65,22 @@ function JournalForm({ onSubmit }) {
 	return (
 		<form className={styles['journal-form']} onSubmit={addJournalItem}>
 			<div>
-				<input ref={titleRef} type='text' onChange={onChange} value={values.title} name='title' className={cn(styles['input-title'], {
-					[styles['invalid']]: !isValid.title
-				})} />
+				<Input type='text' ref={titleRef} onChange={onChange} appearence='title' value={values.title} name='title' isValid={!isValid.title} />
 			</div>
 			<div className={styles['form-row']}>
 				<label htmlFor="date" className={styles['form-label']}>
 					<img src='/calendar.svg' alt='Иконка календаря' />
 					<span>Дата</span>
 				</label>
-				<input ref={dateRef} type='date' onChange={onChange} name='date' value={values.data} id="date" className={cn(styles['input'], {
-					[styles['invalid']]: !isValid.date
-				})} />
+				<Input type='date' ref={dateRef} onChange={onChange} name='date' value={values.date} id="date" isValid={!isValid.title} />
 			</div>
 			<div className={styles['form-row']}>
 				<label htmlFor="tag" className={styles['form-label']}>
 					<img src='/folder.svg' alt='Иконка папки' />
 					<span>Метки</span>
 				</label>
-				<input type='text' onChange={onChange} id="tag" value={values.tag} name='tag' className={styles['input']} />
-			</div>
+
+				<Input type='text' onChange={onChange} id="tag" value={values.tag} name='tag' />			</div>
 
 			<textarea ref={postRef} onChange={onChange} value={values.post} name="post" id="" cols="30" rows="10" className={cn(styles['input'], {
 				[styles['invalid']]: !isValid.post
